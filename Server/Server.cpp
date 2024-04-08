31148e2fe39dea10d7279dcaebf45ffd7af7bd98
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-04-08 14:55:25 +0900

    LockGuard, SpinLock, DaedLock

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -4,37 +4,61 @@ using namespace std;
 #include <vector>
 #include <windows.h>
 #include <atomic>
+#include <mutex>
+#include <map>
 
-atomic<int> sum = 0;
-
-void Add()
+class User
 {
-	for (int i = 0; i < 1'000'000; i++)
+
+};
+
+class UserManager
+{
+public:
+	User* GetUser(int id)
 	{
-		sum++;
+		unique_lock<mutex> guard(_lock);
+
+		auto findIt = _users.find(id);
+		if (findIt == _users.end())
+			return nullptr;
+
+		return findIt->second;
+	}
+
+private:
+	map<int, User*> _users;
+	mutex _lock;
+};
+
+mutex m1;
+mutex m2;
+
+void Thread_1()
+{
+	for (int i = 0; i < 10'000; i++)
+	{
+		lock_guard<mutex> lockGuard1(m1);
+		lock_guard<mutex> lockGuard2(m2);
 	}
 }
 
-void Sub()
+void Thread_2()
 {
-	for (int i = 0; i < 1'000'000; i++)
+	for (int i = 0; i < 10'000; i++)
 	{
-		sum--;
+		lock_guard<mutex> lockGuard2(m2); 
+		lock_guard<mutex> lockGuard1(m1);
 	}
 }
 
 int main()
-{ 
-	sum.store(20); 
-
-	int temp = sum.exchange(10);
-	int prev = sum.fetch_add(30);
-
-	thread t1(Add);
-	thread t2(Sub);
+{
+	thread t1(Thread_1);
+	thread t2(Thread_2);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;
+	cout << "Jobs Done" << endl;
 }
